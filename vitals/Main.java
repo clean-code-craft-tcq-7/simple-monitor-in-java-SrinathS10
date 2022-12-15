d5076505020ef7f0ac5b7e56f143a185940fb288
Srinath Sridhar <Sridhar.Srinath@in.bosch.com>
2022-12-15 13:09:56 +0530

    additional test cases added - review comments

--- vitals/Main.java
+++ vitals/Main.java
@@ -3,7 +3,7 @@ package vitals;
 public class Main {
     private static BatteryManagementSystem BMS;
 
-    public static void testBatteryStatus(){
+    public static void testBatteryStatus() {
         assert(BMS.isBatteryOkay(25F, 70, 0.7f));
         assert(!BMS.isBatteryOkay(50F, 85, 0.0f));
 
@@ -19,10 +19,43 @@ public class Main {
         // threshold
         assert (BMS.isBatteryOkay(BMSLimits.minTemperature, BMSLimits.minSoc, BMSLimits.maxChargeRate));
         assert (BMS.isBatteryOkay(BMSLimits.maxTemperature, BMSLimits.maxSoc, BMSLimits.maxChargeRate));
+
+        // additional tests
+        assert (!BMS.isBatteryOkay(BMSLimits.minTemperature,-5,0));
+        assert (!BMS.isBatteryOkay(BMSLimits.maxTemperature,50,1.5f));
+        assert (!BMS.isBatteryOkay(-280.0f,BMSLimits.minSoc,0.1f));
+        assert (BMS.isBatteryOkay(12f,60,0.4f));
     }
+
+    public static void testPropertyStatus() {
+        //below threshold
+        assert (!BMS.isSocValid(BMSLimits.minSoc - 0.1f));
+        assert (!BMS.isTemperatureValid(BMSLimits.minTemperature - 0.01f));
+
+        //above threshold
+        assert (!BMS.isSocValid(BMSLimits.maxSoc + 0.1f));
+        assert (!BMS.isChargeRateValid(BMSLimits.maxChargeRate + 0.01f));
+        assert (!BMS.isTemperatureValid(BMSLimits.maxTemperature + 0.01f));
+
+        //threshold
+        assert (BMS.isChargeRateValid(BMSLimits.maxChargeRate));
+        assert (BMS.isTemperatureValid(BMSLimits.maxTemperature));
+        assert (BMS.isTemperatureValid(BMSLimits.maxTemperature));
+        assert (BMS.isSocValid(BMSLimits.minSoc));
+        assert (BMS.isSocValid(BMSLimits.maxSoc));
+
+        //additional tests
+        assert (BMS.isSocValid(50));
+        assert (BMS.isTemperatureValid(10.5f));
+        assert (!BMS.isSocValid(0));
+        assert (!BMS.isChargeRateValid(1.2f));
+        assert (!BMS.isTemperatureValid(105f));
+    }
+
 
     public static void main(String[] args) {
         BMS = new BMSImplementation();
+        testPropertyStatus();
         testBatteryStatus();
 
         // for printing battery status with I/O
